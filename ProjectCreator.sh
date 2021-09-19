434ce194639ca8c88d18b0cbdc318ea7901de560
Caglayan DOKME <caglayandokme@gmail.com>
2021-09-19 13:54:09 +0300

    Terminal inputs covered
    The Tickle file name and Vivado path can be fetched from the command line input arguments

--- ProjectCreator.sh
+++ ProjectCreator.sh
@@ -3,10 +3,32 @@
 # Date      : September 19, 2020
 # Note      : User must specify a valid tickle file name
 # Author    : Caglayan DOKME, caglayan.dokme@gmail.com
+# Usage     : User can either modify the script file to invoke the Vivado with a custom TCL file
+#           : or give the TCL file as an input argument(Better with full path)
+#           : The Vivado application version and installiation path can vary for hos devices
+#           : Therefore, it can also be given as an input or modified inside the script file
 
-# User specified variables
-tclfile=BspDevelopment.tcl                  # Main tickle file name to be used initially in Vivado
+# User specified variables(Can be modified using the command line input arguments)
+tclfile=System.tcl                          # Main tickle file name to be used initially in Vivado
 vivadoPath=/tools/Xilinx/Vivado/2017.4      # Path of the Vivado in the host device
+
+# Check for command line input arguments
+# First input argument represents the tickle file name(can be full path)
+if [ $# -gt 0 ]; then
+    tclfile=$1
+
+    # Second input argument represents the full path to Vivado
+    if [ $# -gt 1 ]; then
+        vivadoPath=$2
+
+        # Cannot accept more than 2 input arguments
+        if [ $# -gt 2 ]; then
+            echo "ERROR: Too many($#) input arguments!"
+
+            exit
+        fi
+    fi
+fi
 
 # Check if the given tickle file exists
 if [ -e "$tclfile" ]; then
