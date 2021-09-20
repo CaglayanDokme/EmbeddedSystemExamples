c3ace96b4ab9e11a1d11f547cdbd1f0eaa65ef95
Caglayan DOKME <caglayandokme@gmail.com>
2021-09-20 09:45:14 +0300

    Input argument parsing feature added.

--- ProjectCreator.bat
+++ ProjectCreator.bat
@@ -1,17 +1,41 @@
 :: Brief	: This is a batch file for invoking the Vivado and initially run a tickle file in it
-::			: The batch file is only compatible with Windows PCs
-::			: It's been tested in both Windows 7 and Windows 10
-:: Note		: User must specify a valid tickle file name
-::			: Do not forget to modify the Vivado version also
+::		    : The batch file is only compatible with Windows PCs
+::		    : It's been tested in both Windows 7 and Windows 10
+:: Note		: User must specify a valid tickle file name and a Vivado version
+::		    : It can also be done using the command line input parameters
+:: Usage	: You can either specift the Vivado version or not
+::		    : If you already modified the batch script, then just run it
+::		    : > .\Magic.bat
+::		    : >	.\Magic.bat BspDevelopment.tcl
+::		    : > .\Magic.bat BspDevelopment.tcl 2020.1
 :: Author 	: Caglayan DOKME, caglayandokme@gmail.com
-:: Date		: September 13, 2020
+:: Date		: September 13, 2021 -> Created.
+::			: September 19, 2021 -> Input argument parsing feature added.
 
 @ECHO OFF
 
 :: User specified variables
 :: Main tickle file name to be used first in Vivado
-set tclfile=BspDevelopment.tcl
+set tclfile=System.tcl
 set vivadoVersion=2017.2
+
+:: Parse the program arguments
+:: First input is the tickle file name
+:: Second input is the Vivado version
+if not "%1" == "" (
+	set tclFile=%1
+	
+	if not "%2" == "" (
+		set vivadoVersion=%2
+		
+		if not "%3" == "" (
+			msg Too many input arguments given.
+			
+			timeout 3 >nul
+			exit
+		)
+	)
+) 
 
 :: Derived application path
 set vivadoPath=C:\Xilinx\Vivado\%vivadoVersion%\bin
@@ -48,5 +72,5 @@ start %vivadoPath%\unwrapped\win64.o\vvgl.exe ^
 ECHO Vivado invoked, the program will run the given TCL file
 ECHO Caglayan DOKME salutes you :)
 
-timeout 3 >nul
+timeout 5 >nul
 exit
